68b95377bc5507fb0d3d13bde107c1c3a8529900
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-11-18 12:24:09 +0900

    Update update.py

--- update.py
+++ update.py
@@ -6,6 +6,8 @@ from urllib import parse
 HEADER="""#
 # Code-Tree 문제 풀이 목록
 """
+[![코드트리|실력진단-wndid2008](https://banner.codetree.ai/v1/banner/wndid2008)](https://www.codetree.ai/profiles/wndid2008)
+
 
 def main():
   content = ""
