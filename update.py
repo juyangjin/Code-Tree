669750f3d9f1eabb23cf3dd643578bc9f4132176
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-11-18 12:42:26 +0900

    Update update.py

--- update.py
+++ update.py
@@ -6,6 +6,10 @@ import subprocess
 
 HEADER = """#
 # 코드트리 문제 풀이 목록
+
+코드트리 실력진단  
+[![코드트리 실력진단](https://banner.codetree.ai/v1/banner/wndid2008)](https://www.codetree.ai/profiles/wndid2008)
+
 """
 
 def generate_readme():
