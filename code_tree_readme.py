aa1092e39a01d7db873faffc4196246ca36c2800
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-11-18 14:23:34 +0900

    Update code_tree_readme.py

--- code_tree_readme.py
+++ code_tree_readme.py
@@ -5,6 +5,7 @@ from urllib import parse
 
 HEADER = """#
 # 코드트리 문제 풀이 목록
+[![코드트리|실력진단-wndid2008](https://banner.codetree.ai/v1/banner/wndid2008)](https://www.codetree.ai/profiles/wndid2008)
 """
 
 def generate_readme():
