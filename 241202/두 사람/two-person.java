fceb6d3074deb3ed9c11f5f1f1020416c1a4530c
Juyang_Jin <wndid2008@gmail.com>
2024-12-02 10:32:26 +0900

    Title: 두 사람, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241202/두 사람/two-person.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+
+        String one = sc.nextLine();
+        String two = sc.nextLine();
+
+        String[] str_one = one.split(" ");
+        String[] str_two = one.split(" ");
+        int tmp = 0;
+
+        if(one.contains("M")){
+            tmp = Integer.parseInt(str_one[0]);
+            if(tmp > 19) answer = 1;
+        }
+        if(two.contains("M")){
+            tmp = Integer.parseInt(str_two[0]);
+            if(tmp > 19) answer = 1;
+        }
+
+        System.out.print(answer);
+    }
+}
