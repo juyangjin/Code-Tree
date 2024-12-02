361246d5a11ce524fcf33d245a0977f21ae30f67
Juyang_Jin <wndid2008@gmail.com>
2024-12-02 10:50:31 +0900

    Title: 두 사람, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241202/두 사람/two-person.java
+++ 241202/두 사람/two-person.java
@@ -5,27 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int answer = 0;
 
-        String one = sc.nextLine();
-        String two = sc.nextLine();
+        String[] a = sc.nextLine().split(" ");
+        String[] b = sc.nextLine().split(" ");
 
-        String[] str_one = one.split(" ");
-        String[] str_two = one.split(" ");
-        int tmp = 0;
-
-        if(one.contains("W") && two.contains("w")){
-            answer = 0;
-        }else{
-            if(one.contains("M")){
-            tmp = Integer.parseInt(str_one[0]);
-            if(tmp >= 19) answer = 1;
-            else answer = 0;
-        }
-        tmp = 0;
-        if(two.contains("M")){
-            tmp = Integer.parseInt(str_two[0]);
-            if(tmp >= 19) answer = 1;
-            else answer = 0;
-        }
+        int i_a = Integer.parseInt(a[0]);
+        int i_b = Integer.parseInt(b[0]);
+        
+        if((a[1].contains("M") && i_a >= 19) || (b[1].contains("M") && i_b >= 19)){
+            answer = 1;
         }
         
         System.out.print(answer);
